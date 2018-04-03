b474b1c8f90f259f095053d4a266859e86fcfa16
Isaac Wong <isaac_wch@outlook.com>
2018-04-04 01:02:31 +0800

    Added the date plugin and removed the hard-coded values

--- notepad/src/App.vue
+++ notepad/src/App.vue
@@ -6,61 +6,75 @@
         <select class="mb-3" v-model="selectedtype">
           <option v-for="nt in notetypes" :key="nt">{{nt}}</option>
         </select>
+        <div class="datepicker-trigger" v-show="selectedtype === 'To Do'">
+          <input
+            type="text"
+            id="datepicker-trigger"
+            placeholder="Select dates"
+            :value="formatDates(dateOne, dateTwo)"
+          >
+          <AirbnbStyleDatepicker
+            :trigger-element-id="'datepicker-trigger'"
+            :mode="'single'"
+            :fullscreen-mobile="false"
+            :date-one="dateOne"
+            :months-to-show="1"
+            @date-one-selected="val => { dateOne = val }"
+          />
+        </div>
         <br>
         <textarea class="msg" v-model="message" placeholder="please enter the information here......"></textarea>
         <br>
-        <button class="btn btn-primary">Submit</button>
+        <button class="btn btn-primary" @click="addNote(selectedtype,dateOne,message)">Submit</button>
+        <p>{{dateOne}}</p>
       </div>
     </div>
    <div class="list-group w-25">
-    <div class="w-100 mb-3 list-group-item" v-for="note in notes" :key="note.id">
-      <h5>{{note.nt}}</h5>
+    <div class="w-100 mb-3 list-group-item" v-for="note in notes">
+      <h5>{{note.msgType}}</h5>
       <p>{{note.msg}}</p>
       <small>{{note.date}}</small>
     </div>
   </div>
-  <!--<div class="datepicker-trigger">
-      <input
-        type="text"
-        id="datepicker-trigger"
-        placeholder="Select dates"
-        :value="formatDates(dateOne, dateTwo)"
-      >
-
-      <AirbnbStyleDatepicker
-        :trigger-element-id="'datepicker-trigger'"
-        :mode="'range'"
-        :fullscreen-mobile="true"
-        :date-one="dateOne"
-        :date-two="dateTwo"
-        @date-one-selected="val => { dateOne = val }"
-        @date-two-selected="val => { dateTwo = val }"
-      />
-    </div>-->
   </div>
 </template>
 
 <script>
+import format from 'date-fns/format'
+
 export default {
   name: 'app',
   data () {
     return {
+      dateFormat: 'D MMM',
+      dateOne: '',
+      dateTwo: '',
+      message: '',
       notetypes: ['To Do','Expense'],
       selectedtype: [],
-      notes: [
-        {
-          id: 1,
-          nt: "To Do",
-          msg: "Check sth",
-          date: "15/11/2016 04:34 AM",
-        },
-         {
-          id: 2,
-          nt: "To Do",
-          msg: "Check sth",
-          date: "15/11/2016 04:34 AM",
-        }
-      ],
+      isshowdp: false,
+      notes: [],
+    }
+  },
+  methods: {
+    formatDates(dateOne, dateTwo) {
+      let formattedDates = ''
+      if (dateOne) {
+        formattedDates = format(dateOne, this.dateFormat)
+      }
+      if (dateTwo) {
+        formattedDates += ' - ' + format(dateTwo, this.dateFormat)
+      }
+      return formattedDates
+    },
+    showdp(selectedtype){
+      console.log("you have chosen " + selectedtype);
+      if(selectedtype === "To Do"){
+          isshowdp = true;
+      }
+    },
+    addNote(selectedtype,dateOne,message){
+      this.notes.push({msgType: selectedtype, date: dateOne, msg: message });
     }
   }
 }
